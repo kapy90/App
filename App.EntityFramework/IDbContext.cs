0d617ea719a75397dbed8d6bc531ee1e47bdca2b
kapy <2955005601@qq.com>
2020-03-04 13:26:51 +0800

    add App.EntityFramework

---
+++ App.EntityFramework/IDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.Linq;
+
+namespace App.EntityFramework
+{
+    /// <summary>
+    /// Represents DB context
+    /// </summary>
+    public partial interface IDbContext
+    {
+        #region Methods
+
+        /// <summary>
+        /// Creates a DbSet that can be used to query and save instances of entity
+        /// </summary>
+        /// <typeparam name="TEntity">Entity type</typeparam>
+        /// <returns>A set for the given entity type</returns>
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        /// <summary>
+        /// Saves all changes made in this context to the database
+        /// </summary>
+        /// <returns>The number of state entries written to the database</returns>
+        int SaveChanges();
+
+        /// <summary>
+        /// Generate a script to create all tables for the current model
+        /// </summary>
+        /// <returns>A SQL script</returns>
+        string GenerateCreateScript();
+
+        /// <summary>
+        /// Executes the given SQL against the database
+        /// </summary>
+        /// <param name="sql">The SQL to execute</param>
+        /// <param name="doNotEnsureTransaction">true - the transaction creation is not ensured; false - the transaction creation is ensured.</param>
+        /// <param name="timeout">The timeout to use for command. Note that the command timeout is distinct from the connection timeout, which is commonly set on the database connection string</param>
+        /// <param name="parameters">Parameters to use with the SQL</param>
+        /// <returns>The number of rows affected</returns>
+        int ExecuteSqlCommand(RawSqlString sql, bool doNotEnsureTransaction = false, int? timeout = null, params object[] parameters);
+
+        /// <summary>
+        /// Detach an entity from the context
+        /// </summary>
+        /// <typeparam name="TEntity">Entity type</typeparam>
+        /// <param name="entity">Entity</param>
+        void Detach<TEntity>(TEntity entity) where TEntity : class;
+
+        #endregion
+        DatabaseFacade Database { get; }
+    }
+}
---
+++ App.EntityFramework/ObjectDbContext.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace App.EntityFramework
+{
+    /// <summary>
+    /// Represents base object context
+    /// </summary>
+    public partial class ObjectDbContext : DbContext, IDbContext
+    {
+        #region Ctor
+
+        public ObjectDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        #endregion
+
+        #region Utilities
+
+        /// <summary>
+        /// Further configuration the model
+        /// </summary>
+        /// <param name="modelBuilder">The builder being used to construct the model for this context</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //dynamically load all entity and query type configurations
+            Assembly.GetExecutingAssembly().GetTypes()
+             .Where(x => x.GetInterfaces().Any(y => y.IsGenericType && y.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>)))
+             .ToList().ForEach(x =>
+             {
+                 dynamic configurationInstance = Activator.CreateInstance(x);
+                 modelBuilder.ApplyConfiguration(configurationInstance);
+             });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Creates a DbSet that can be used to query and save instances of entity
+        /// </summary>
+        /// <typeparam name="TEntity">Entity type</typeparam>
+        /// <returns>A set for the given entity type</returns>
+        public virtual new DbSet<TEntity> Set<TEntity>() where TEntity : class
+        {
+            return base.Set<TEntity>();
+        }
+
+        /// <summary>
+        /// Generate a script to create all tables for the current model
+        /// </summary>
+        /// <returns>A SQL script</returns>
+        public virtual string GenerateCreateScript()
+        {
+            return Database.GenerateCreateScript();
+        }
+
+       
+        /// <summary>
+        /// Executes the given SQL against the database
+        /// </summary>
+        /// <param name="sql">The SQL to execute</param>
+        /// <param name="doNotEnsureTransaction">true - the transaction creation is not ensured; false - the transaction creation is ensured.</param>
+        /// <param name="timeout">The timeout to use for command. Note that the command timeout is distinct from the connection timeout, which is commonly set on the database connection string</param>
+        /// <param name="parameters">Parameters to use with the SQL</param>
+        /// <returns>The number of rows affected</returns>
+        public virtual int ExecuteSqlCommand(RawSqlString sql, bool doNotEnsureTransaction = false, int? timeout = null, params object[] parameters)
+        {
+        
+            //set specific command timeout
+            var previousTimeout = Database.GetCommandTimeout();
+            Database.SetCommandTimeout(timeout);
+
+            var result = 0;
+            if (!doNotEnsureTransaction)
+            {
+                //use with transaction
+                using (var transaction = Database.BeginTransaction())
+                {
+                    result = Database.ExecuteSqlCommand(sql, parameters);
+                    transaction.Commit();
+                }
+            }
+            else
+                result = Database.ExecuteSqlCommand(sql, parameters);
+
+            //return previous timeout back
+            Database.SetCommandTimeout(previousTimeout);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Detach an entity from the context
+        /// </summary>
+        /// <typeparam name="TEntity">Entity type</typeparam>
+        /// <param name="entity">Entity</param>
+        public virtual void Detach<TEntity>(TEntity entity) where TEntity : class
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            var entityEntry = Entry(entity);
+            if (entityEntry == null)
+                return;
+
+            //set the entity is not being tracked by the context
+            entityEntry.State = EntityState.Detached;
+        }
+
+        #endregion
+    }
+}
