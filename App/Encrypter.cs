1a0c52e1ea2ccc82545186266538851c26a13b56
kapy <2955005601@qq.com>
2020-03-06 11:11:55 +0800

    add encrypter

---
+++ App/Encrypter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace App
+{
+    public class Encrypter
+    {
+        public string EncryptByMD5(string input)
+        {
+            MD5 md5Hasher = MD5.Create();
+            byte[] data = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sBuilder = new StringBuilder();
+            //将每个字节转为16进制
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+    }
+}
