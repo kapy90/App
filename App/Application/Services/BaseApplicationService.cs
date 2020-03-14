55bf5da4ded93f6209d3db222cfcd6217ad94b80
kapy <2955005601@qq.com>
2020-03-14 15:53:54 +0800

    add BaseApplicationService for common  method invoking in reference class. currently have User method for Authorization judgment

---
+++ App/Application/Services/BaseApplicationService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace App.Application.Services
+{
+    public abstract class BaseApplicationService : IApplicationService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public BaseApplicationService()
+        {
+            _httpContextAccessor = Infrastructure.EngineContext.Current.Resolve<IHttpContextAccessor>();
+        }
+
+        protected ClaimsPrincipal User
+        {
+            get
+            {
+                var user = _httpContextAccessor.HttpContext?.User;
+                if (user == null)
+                {
+                    var claimsIdentity = new ClaimsIdentity();
+                    user = new ClaimsPrincipal(claimsIdentity);
+                }
+                return user;
+            }
+        }
+
+    }
+}
