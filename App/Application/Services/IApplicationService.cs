48a30634a612879608f671fdc94fc623aefc001b
kapy <2955005601@qq.com>
2020-03-15 17:06:04 +0800

    switch ApplicationService DI from ITransientDependency to  IScopedDependency

--- App/Application/Services/IApplicationService.cs
+++ App/Application/Services/IApplicationService.cs
@@ -2,7 +2,7 @@
 
 namespace App.Application.Services
 {
-    public interface IApplicationService : ITransientDependency
+    public interface IApplicationService : IScopedDependency
     {
     }
 }
